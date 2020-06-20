9c1db2c5f7f1306291dc4663258166afe64a480a
Shehu Ibrahim Muhammad <profshehu94@gmail.com>
2020-06-20 01:18:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
-// Import stylesheets
-import './style.css';
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD9r4ZM2lssN-n6kUeWnfVNHw4iOM-h7es",
+    authDomain: "chat-app-e1c17.firebaseapp.com",
+    databaseURL: "https://chat-app-e1c17.firebaseio.com",
+    projectId: "chat-app-e1c17",
+    storageBucket: "chat-app-e1c17.appspot.com",
+    messagingSenderId: "878369522416",
+    appId: "1:878369522416:web:0227306c8d92b71b71f835",
+    measurementId: "G-JJ1SQKRWZ5"
+    aut
+  };
 
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-import * as firebaseui from 'firebaseui';
+// Reference Cloud Firestore
+var db = firebase.firestore();
 
-// Document elements
-const startRsvpButton = document.getElementById('startRsvp');
-const guestbookContainer = document.getElementById('guestbook-container');
+// Get the name for the user
+if (!localStorage.getItem('name')) {
+	name = prompt('What is your name?')
+	localStorage.setItem('name', name)
+} else {
+	name = localStorage.getItem('name')
+}
+document.querySelector('#name').innerText = name
 
-const form = document.getElementById('leave-message');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
+// Change name
+document.querySelector('#change-name').addEventListener('click', () => {
+	name = prompt('What is your name?')
+	document.querySelector('#name').innerText = name
+})
 
-var rsvpListener = null;
-var guestbookListener = null;
+// Send a new chat message
+document.querySelector('#message-form').addEventListener('submit', e => {
+	e.preventDefault();
+	db.collection("messages")
+	.add({
+		name: name,
+		message: document.querySelector('#message-input').value 
+	})
+	.then(function (docRef) {
+		console.log("Document written with ID: ", docRef.id);
+		document.querySelector('#message-form').reset()
+	})
+	.catch(function (error) {
+		console.error("Error adding document: ", error);
+	});
+})
 
-// Add Firebase project configuration object here
-// var firebaseConfig = {};
+// Display chat stream
+db.collection("messages")
+.onSnapshot(function(snapshot) {
+	document.querySelector('#messages').innerHTML = ""
+	snapshot.forEach(function(doc) {
+		var message = document.createElement('div')
+		message.innerHTML = `
+		<p class="name">${doc.data().name}</p>
+		<p>${doc.data().message}</p>
+		`
+		document.querySelector('#messages').prepend(message)
+	});
+});
 
-// firebase.initializeApp(firebaseConfig);
-
-// FirebaseUI config
-const uiConfig = {
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-  signInOptions: [
-    // Email / Password Provider.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl){
-      // Handle sign-in.
-      // Return false to avoid redirect.
-      return false;
-    }
-  }
-};
-
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+// Remove all chat messages
+document.querySelector('#clear').addEventListener('click', () => {
+	db.collection("messages")
+    .get()
+    .then(function(snapshot) {
+        snapshot.forEach(function(doc) {
+			db.collection("messages").doc(doc.id).delete().then(function() {
+				console.log("Document successfully deleted!");
+			}).catch(function(error) {
+				console.error("Error removing document: ", error);
+			});
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+})
